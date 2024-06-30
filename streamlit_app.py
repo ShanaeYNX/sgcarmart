a8c4572fa24e3c9fec309e0248ee73fb30852fdc
ShanaeYNX <shanaeyap@gmail.com>
2024-06-30 23:22:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,19 +5,7 @@ from datetime import date
 import pickle
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
-import gzip
-import os
-import sklearn
-import joblib
 
-# file_path = 'final_model.pkl.gz'
-
-st.success(sklearn.__version__)
-
-# if os.path.exists(file_path):
-#     st.success(f"Found file at {file_path}")
-# else:
-#     st.success(f"File not found at {file_path}")
 
 #train the model
 model = RandomForestRegressor(bootstrap=False, max_depth=15, max_features=0.5, min_samples_leaf=1, min_samples_split=2, n_estimators=200, random_state=2024)
@@ -26,15 +14,11 @@ X_train = pd.read_csv('sg_used_cars_X_train.csv')
 y_train = pd.read_csv('sg_used_cars_y_train.csv')
 model.fit(X_train, y_train)
 
-
 st.write("""
 # Predicting Used Car Prices
 This app predicts the **recommended car listing price** and its **yearly depreciation** using features input via the **side panel** 
 """)
-# Load the model
-# with gzip.open(file_path, 'rb') as f:
-#    model = pickle.load(f)
-# loaded_model = joblib.load('final_model.joblib')
+
 # Load the dataframe skeleton for prediction
 df_skeleton = pd.read_csv('df_skeleton.csv', index_col = 0)
 # Load the brand_list
